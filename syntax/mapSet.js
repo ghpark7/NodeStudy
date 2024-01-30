51538cd2961317503786ff870864fce87833b3c9
Geonhwi Park <geonhwipark715@gmail.com>
2024-01-30 15:32:44 +0900

    Update mapSet.js
    Set의 다양한 활용법 학습

--- syntax/mapSet.js
+++ syntax/mapSet.js
@@ -1,3 +1,5 @@
+// Map
+
 const m = new Map();
 
 m.set('a', 'b'); // set(key, value)로 Map에 속성 추가
@@ -25,3 +27,29 @@ console.log(m.has(d)); // true
 m.delete(d); // delete(key)로 Map의 속성 삭제
 m.clear(); // clear()로 Map의 모든 속성 삭제
 console.log(m.size); // 0
+
+// Set
+
+const s = new Set();
+
+s.add(false); // add(value)로 Set에 속성 추가
+s.add(1);
+s.add('1');
+s.add(1); // 중복된 값은 추가되지 않음
+s.add(2);
+
+console.log(s.size); // 중복된 값은 추가되지 않으므로 4
+
+s.has(1); // has(value)로 Set에 특정 속성이 있는지 확인
+console.log(s.has(1)); // true
+
+for (const value of s) {
+  console.log(value); // false, 1, '1', 2
+} // 속성 간의 순서도 보장
+
+s.forEach((value) => {
+  console.log(value); // false, 1, '1', 2
+}); // forEach도 사용 가능
+
+s.delete(1); // delete(value)로 Set의 속성 삭제
+s.clear(); // clear()로 Set의 모든 속성 삭제
