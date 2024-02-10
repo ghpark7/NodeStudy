48368ecebad59ae5ae1478b575ead30114190723
Geonhwi Park <ghpark715@gmail.com>
2024-02-10 20:30:24 +0900

    Update spawn.js
    간단한 오류 수정

--- basic/spawn.js
+++ basic/spawn.js
@@ -1,6 +1,6 @@
 const spawn = require('child_process').spawn;
 
-const process = spawn('python', ['test.py']);
+const process = spawn('python3', ['test.py']);
 
 process.stdout.on('data', function(data) {
   console.log(data.toString());
